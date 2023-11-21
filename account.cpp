fc360959c98d04b9e9762ca1b8b0052ca7d5cf3f
linann260 <106176130+linann260@users.noreply.github.com>
2023-11-21 18:16:45 +0000

    formatted code

--- account.cpp
+++ account.cpp
@@ -9,7 +9,8 @@ Purpose: Creates Account objects that can deposit, withdrawal, and transfer mone
 
 using namespace std;
 
-class Account{
+class Account
+{
     /*
     Parent class that has an account number, holder, and balance.
     Functionalities include displaying account details, depositing, and withdrawing money.
@@ -25,64 +26,74 @@ class Account{
         Account(string number, string holder, double balance) // constructor
             : accountNumber(number), accountHolder(holder), accountBalance(balance){}
 
-        string getNumber(){
+        string getNumber()
+        {
             /*
             Helper function that returns the account number.
             */
             return accountNumber;
         }
 
-        string getHolder(){
+        string getHolder()
+        {
             /*
             Helper function that returns the account holder.
             */
             return accountHolder;
         }
 
-        double getBalance(){
+        double getBalance()
+        {
             /*
             Helper function that returns the account balance.
             */
             return accountBalance;
         }
 
-        virtual string getAccountType() const{
+        virtual string getAccountType() const
+        {
             /*
             Helper function that returns the account type.
             */
             return "Generic";
         }
 
-        void displayDetails(){
+        void displayDetails()
+        {
             /*
             Function that displays the details of the account using the overloading << operator.
             */
             cout << *this;
         }
 
-        void deposit(double amount){
+        void deposit(double amount)
+        {
             /*
             Function that deposits money into account.
             */
             accountBalance += amount;
         }
 
-        bool withdrawal(double amount){
+        bool withdrawal(double amount)
+        {
             /*
             Function that withdrawals money from account while ensuring sufficient funds. Returns a bool.
             */
-            if (amount > 0 && amount <= accountBalance){
+            if (amount > 0 && amount <= accountBalance)
+            {
                 accountBalance -= amount; // money is withdrawn if funds are sufficient
                 return true;
             }
 
-            else {
+            else
+            {
                 cout << "Account has insufficient funds." << endl;
                 return false;
             }
         }
 
-        friend ostream &operator<<(ostream& COUT, Account& acc) {
+        friend ostream &operator<<(ostream& COUT, Account& acc)
+        {
             /*
             Operator << overloading. Displays account details.
             */
@@ -94,7 +105,8 @@ class Account{
 };
 
 
-class SavingsAccount: public Account{
+class SavingsAccount: public Account
+{
     /*
     Child class of Account.
     SavingsAccount has an interest rate in addition to derived variables from Account.
@@ -110,21 +122,24 @@ class SavingsAccount: public Account{
 
         double accountMinimum = 100; // set minimum balance
 
-        string getAccountType() const override{
+        string getAccountType() const override
+        {
             /*
             Helper function that returns the account type. Overrides Account method.
             */
             return "Savings";
         }
 
-        double getInterestRate() {
+        double getInterestRate()
+        {
             /*
             Helper function that returns the interest rate.
             */
             return interestRate;
         }
 
-        void displayDetails(){
+        void displayDetails()
+        {
             /*
             Helper function that displays account details using operator << overloading from Account.
             */
@@ -132,21 +147,25 @@ class SavingsAccount: public Account{
             cout << "   Interest Rate: " << fixed << setprecision(2) << interestRate << "%" << endl << endl;
         }
 
-        bool withdrawal(double amount){
+        bool withdrawal(double amount)
+        {
              /*
             Helper function that withdrawals from account while ensuring sufficient funds (minimum balance). Returns bool.
             */
-            if (amount > 0 && accountBalance - amount >= accountMinimum){
+            if (amount > 0 && accountBalance - amount >= accountMinimum)
+            {
                 accountBalance -= amount; // money is withdrawn if funds are sufficient
                 return true;
             }
-            else{
+            else
+            {
                 cout << "Savings had insufficient Funds. Minimum balance is $" << accountMinimum << "." << endl;
                 return false;
             }
         }
 
-        SavingsAccount operator+(Account &other){
+        SavingsAccount operator+(Account &other)
+        {
              /*
             Operator + overloading that transfers money between accounts.
             Money is transfered into calling object. Calling object is returned.
@@ -154,18 +173,21 @@ class SavingsAccount: public Account{
             double transfer = 300; // set transfer amount
 
             // Transaction is performed if the other account has sufficient funds.
-            if (other.withdrawal(transfer)){
+            if (other.withdrawal(transfer))
+            {
                 accountBalance += 300;
                 return (*this);
             }
 
-            else{
+            else
+            {
                 return (*this);
             }
         }
 };
 
-class CurrentAccount : public Account{
+class CurrentAccount : public Account
+{
     /*
     Child class of Account.
     CurrentAccount has an overdraft limit in addition to derived variables from Account.
@@ -179,14 +201,16 @@ class CurrentAccount : public Account{
         CurrentAccount(string number, string holder, double balance, double limit)
             : Account(number, holder, balance), overdraftLimit(limit){}
 
-        string getAccountType() const override{
+        string getAccountType() const override
+        {
             /*
             Helper function that returns the account type. Overrides Account method.
             */
             return "Current";
         }
 
-        void displayDetails(){
+        void displayDetails()
+        {
              /*
             Helper function that displays account details using operator << overloading from Account.
             */
@@ -198,35 +222,42 @@ class CurrentAccount : public Account{
             /*
             Helper function that withdrawals from account while ensuring sufficient funds (overdrafting limit). Returns bool.
             */
-            if (amount > 0 && accountBalance - amount >= -overdraftLimit){
+            if (amount > 0 && accountBalance - amount >= -overdraftLimit)
+            {
                 accountBalance -= amount; // money is withdrawn if funds are sufficient
                 return true;
             }
-            else{
+            
+            else
+            {
                 cout << "Current has insufficient funds. Overdrafting limit is " << overdraftLimit << "." << endl;
                 return false;
             }
         }
 
-        CurrentAccount operator+(Account &other){
+        CurrentAccount operator+(Account &other)
+        {
             /*
             Operator + overloading that transfers money between accounts.
             Money is transfered into calling object. Calling object is returned.
             */
             double transfer = 300; // set transfer amount
             // Transaction is performed if the other account has sufficient funds.
-            if (other.withdrawal(transfer)){
+            if (other.withdrawal(transfer))
+            {
                 accountBalance += 300;
                 return (*this);
             }
 
-            else{
+            else
+            {
                 return (*this);
             }
         }
 };  
 
-int main(){
+int main()
+{
     SavingsAccount savings("S123", "John Doe", 1000, 0.02);
     CurrentAccount current("C456", "Jane Doe", 2000, 500);
 
